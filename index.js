01eb14f30011ce4d4466e97ec4c23cb6c3d88973
jona415 <65192440+jona415@users.noreply.github.com>
2020-06-03 23:32:12 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// Importamos las dependencias
+const express = require("express");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+//Creamos una constante para el valor del puerto
+const PUERTO = process.env.PORT 3000;
